dfc8c7d5093839bf4a924f9ac429c886312b14d3
rapuska <mikalajsiatkouski@gmail.com>
2024-07-22 22:45:40 +0300

    adjust speed

--- src/common/constants.rs
+++ src/common/constants.rs
@@ -3,7 +3,7 @@ pub mod player {
 
     pub const SIZE: f32 = 0.25;
     pub const SPAWN_POSITION: Vec3 = Vec3::new(-5.0, 5.0, 0.0);
-    pub const MOVEMENT_SPEED: f32 = 80.0;
+    pub const MOVEMENT_SPEED: f32 = 300.0;
     
     pub const COLLIDER_HEIGHT: f32 = 2.5;
     pub const COLLIDER_RADIUS: f32 = 1.5;
